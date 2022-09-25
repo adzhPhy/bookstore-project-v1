ff09e8edba406c654651df9c9a82b4d1e8d3bad5
Ali-Dzhan Ali <adzh.aliali@gmail.com>
2022-09-25 13:54:03 +0300

    'ch14'

--- books/tests.py
+++ books/tests.py
@@ -1,11 +1,11 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.urls import reverse
 from .models import Book, Review
 # Create your tests here.
 
 class BookTests(TestCase):
-    
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(
@@ -13,6 +13,8 @@ class BookTests(TestCase):
             email='reviewuser@email.com',
             password='testpass123'
         )
+        cls.special_permission = Permission.objects.get(codename='special_status')
+
         cls.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
@@ -28,16 +30,31 @@ class BookTests(TestCase):
         self.assertEqual(f"{self.book.title}", "Harry Potter")
         self.assertEqual(f"{self.book.author}", "JK Rowling")
         self.assertEqual(f"{self.book.price}", "25.00")
-    def test_book_list_view(self):
+    
+    def test_book_list_view_for_logged_in_user(self): # new
+        self.client.login(email="reviewuser@email.com", password="testpass123")
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, "books/book_list.html")
-    def test_book_detail_view(self):
+
+    def test_book_list_view_for_logged_out_user(self): # new
+        self.client.logout()
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 302) # new
+        self.assertRedirects(
+        response, "%s?next=/books/" % (reverse("account_login")))
+        response = self.client.get(
+        "%s?next=/books/" % (reverse("account_login")))
+        self.assertContains(response, "Log In")
+    
+    def test_book_detail_view_with_permissions(self): # new
+        self.client.login(email="reviewuser@email.com", password="testpass123")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get("/books/12345/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
-        self.assertContains(response, "Great book!") # new
+        self.assertContains(response, "Great book!")
         self.assertTemplateUsed(response, "books/book_detail.html")
